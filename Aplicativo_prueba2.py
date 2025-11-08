36b171ce81d3d10eddf758dabe3907ec85210add
RenzoZegarra <renzolzegarra@gmail.com>
2025-11-07 22:06:15 -0500

    Se hicieron comentarios de los botones.

--- Aplicativo_prueba2.py
+++ Aplicativo_prueba2.py
@@ -82,7 +82,7 @@ def mostrar_rutas_con_mejor_rendimiento():
     except Exception as e:
         messagebox.showerror("Error", f"No se pudo obtener datos:\n{e}")
 
-#Función Consulta 5
+#Función Consulta 5: 
 def mostrar_vehiculos_modelo():
     try:
         cursor.execute("SELECT v.placa, v.marca, m.modelo_descripcion, v.centro_costos FROM Vehiculo v INNER JOIN Modelo m ON v.id_modelo = m.id_modelo;")
@@ -225,7 +225,7 @@ titulo = tk.Label(
 )
 titulo.pack(pady=10)  # separa del resto de widgets
 
-
+#BOTON DE CONEXION DE BD
 btn_conectar = tk.Button(ventana, text="Conectar a BD", command=conectar_bd, bg="#4CAF50", fg="white")
 btn_conectar.pack(pady=5)
 
@@ -248,43 +248,57 @@ titulo = tk.Label(
 )
 titulo.pack(pady=10)  # separa del resto de widgets
 
+#BOTONES DE REPORTE/CONSULTA
+
+#Consulta 1: Mostrar vehículos con conductor asignado
 btn_consulta1 = tk.Button(ventana, text="Mostrar vehículos con conductor asignado", command=mostrar_Vehiculos_con_conductor, bg="#36f4ab", fg="white")
 btn_consulta1.pack(pady=5)
 
+#Consulta 2: Mostrar las rutas activas.
 btn_consulta2 = tk.Button(ventana, text="Mostrar las rutas activas", command=mostrar_rutas_activas, bg="#e32813", fg="white")
 btn_consulta2.pack(pady=5)
 
+#Consulta 3: Mostrar detalles de las rutas.
 btn_consulta3 = tk.Button(ventana, text="Mostrar detalles de las rutas", command=mostrar_detalles_rutas, bg="#e32813", fg="white")
 btn_consulta3.pack(pady=5)
 
+#Consulta 4: Mostrar las 3 rutas con el mejor rendimiento.
 btn_consulta4 = tk.Button(ventana, text="Mostrar las 3 rutas con el mejor rendimiento", command=mostrar_rutas_con_mejor_rendimiento, bg="#e32813", fg="white")
 btn_consulta4.pack(pady=5)
 
+#Consulta 5: Mostrar los vehículos con su modelo y centro de costos.
 btn_consulta5 = tk.Button(ventana, text="Mostrar los vehículos con su modelo y centro de costos.", command=mostrar_vehiculos_modelo, bg="#e32813", fg="white")
 btn_consulta5.pack(pady=5)
 
+#Consulta 6: Mostrar los modelos más usados por vehículos.
 btn_consulta6 = tk.Button(ventana, text="Mostrar los modelos más usados por vehículos.", command=mostrar_modelos_mas_usados, bg="#e32813", fg="white")
 btn_consulta6.pack(pady=5)
 
+#Consulta 7: Mostrar el consumo promedio por modelo en todas las rutas.
 btn_consulta7 = tk.Button(ventana, text="Mostrar el consumo promedio por modelo en todas las rutas", command=mostrar_consumo_promedio_modelos, bg="#e32813", fg="white")
 btn_consulta7.pack(pady=5)
 
+#Consulta 8: Mostrar los vehículos con tonelaje superior a 8 toneladas.
 btn_consulta8 = tk.Button(ventana, text="Mostrar los vehículos con tonelaje superior a 8 toneladas", command=mostrar_vehiculos_pesados, bg="#e32813", fg="white")
 btn_consulta8.pack(pady=5)
 
+#Consulta 9: Mostrar las rutas por tipo (local o provincial) con cantidad total.
 btn_consulta9 = tk.Button(ventana, text="Mostrar las rutas por tipo(local o provincial) con cantidad total", command=mostrar_rutas_por_tipo, bg="#e32813", fg="white")
 btn_consulta9.pack(pady=5)
 
+#Consulta 10: Mostrar las rutas inactivas.
 btn_consulta10 = tk.Button(ventana, text="Mostrar las rutas inactivas", command=mostrar_rutas_inactivas, bg="#e32813", fg="white")
 btn_consulta10.pack(pady=5)
 
+#Consulta 11: Mostrar los vehículos agrupados por modelo.
 btn_consulta11 = tk.Button(ventana, text="Mostrar los vehículos agrupados por modelo.", command=mostrar_vehiculos_por_modelo, bg="#e32813", fg="white")
 btn_consulta11.pack(pady=5)
 
+#Consulta 12: Buscar un modelo específico.
 btn_consulta12 = tk.Button(ventana, text="Buscar un modelo específico.", command=buscar_modelo_especifico, bg="#e32813", fg="white")
 btn_consulta12.pack(pady=5)
 
-
+#BOTON DE CERRAR CONEXION CON LA BASE DE DATOS
 btn_cerrar = tk.Button(ventana, text="Cerrar Conexión", command=cerrar_conexion, bg="#f44336", fg="white")
 btn_cerrar.pack(pady=5)
 
